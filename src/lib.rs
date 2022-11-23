da6d03e4f639bb18e7be1af0441d3c83d0387c76
Sebastian Messmer <messmer@cryfs.org>
2022-11-22 16:58:34 -0800

    More crate level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,40 @@
 //! The [lockable](https://crates.io/crates/lockable) library offers thread-safe
-//! HashMap (see [LockableHashMap]) and LruCache (see [LockableLruCache]) types
-//! where individual keys can be locked/unlocked, even if there is no entry for
-//! this key in the map. This is great for synchronizing access to an underlying
-//! key-value store or for building cache data structures on top of such
-//! key-value stores.
+//! HashMap (see [LockableHashMap](crate::LockableHashMap)) and LruCache
+//! (see [LockableLruCache](crate::LockableLruCache)) types where individual keys
+//! can be locked/unlocked, even if there is no entry for this key in the map.
+//!
+//! This can be very useful for synchronizing access to an underlying key-value
+//! store or for building cache data structures on top of such a key-value store.
+//!
+//! ## LRU cache example
+//! This example builds a simple LRU cache and locks some entries.
 //!
 //! ```
 //! use lockable::{AsyncLimit, LockableLruCache};
 //!
 //! let lockable_cache: LockableLruCache<i64, String> = LockableLruCache::new();
+//! # tokio::runtime::Runtime::new().unwrap().block_on(async {
+//! let entry1 = lockable_cache.async_lock(4, AsyncLimit::no_limit()).await?;
+//! let entry2 = lockable_cache.async_lock(5, AsyncLimit::no_limit()).await?;
+//!
+//! // This next line would cause a deadlock or panic because `4` is already locked on this thread
+//! // let entry3 = lockable_cache.async_lock(4, AsyncLimit::no_limit()).await?;
+//!
+//! // After dropping the corresponding guard, we can lock it again
+//! std::mem::drop(entry1);
+//! let entry3 = lockable_cache.async_lock(4, AsyncLimit::no_limit()).await?;
+//! # Ok::<(), lockable::Never>(())}).unwrap();
+//! ```
+//!
+//! ## Lockpool example
+//! This example builds a simple lock pool using the [LockableHashMap](crate::LockableHashMap)
+//! data structure. A lock pool is a pool of keyable locks. In this example, the entries
+//! don't have a value assigned to them and the lock pool is only used to synchronize
+//! access to some keyed resource.
+//! ```
+//! use lockable::{AsyncLimit, LockableHashMap};
+//!
+//! let lockable_cache: LockableHashMap<i64, ()> = LockableHashMap::new();
 //! # tokio::runtime::Runtime::new().unwrap().block_on(async {
 //! let entry1 = lockable_cache.async_lock(4, AsyncLimit::no_limit()).await?;
 //! let entry2 = lockable_cache.async_lock(5, AsyncLimit::no_limit()).await?;
