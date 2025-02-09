c5343e42960a1f3d672a8d8f4d1a040ad105981d
Sebastian Messmer <smessmer@users.noreply.github.com>
2025-02-09 20:29:02 +0000

    Setup devcontainer

---
+++ .devcontainer/setup_fish.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+set -v
+
+curl -o /tmp/omf_install https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install 
+fish /tmp/omf_install --noninteractive
+
+fish -c "omf install bobthefish"
+
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.1/Hack.tar.xz
+tar -xvf Hack.tar.xz
+rm Hack.tar.xz
+
+fish -c 'set -U theme_nerd_fonts yes'
