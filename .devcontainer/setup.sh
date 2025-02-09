27dd9e2ceebd630bb22aacd35db3819fba15f798
Sebastian Messmer <smessmer@users.noreply.github.com>
2025-02-09 20:46:32 +0000

    devcontainer setup.sh uses paths relative to the script location

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,5 +3,7 @@
 set -e
 set -v
 
-./setup_fish.sh
-./install_rust_utils.sh
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+${SCRIPT_DIR}/setup_fish.sh
+${SCRIPT_DIR}/install_rust_utils.sh
