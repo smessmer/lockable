6e952e53568af28d000c9eecfe92bf5002d5756a
Sebastian Messmer <mail@smessmer.de>
2025-06-08 20:20:51 -0700

    Fix clippy warnings

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,5 +1,6 @@
-use criterion::{Criterion, black_box, criterion_group, criterion_main};
+use criterion::{Criterion, criterion_group, criterion_main};
 use lockable::LockPool;
+use std::hint::black_box;
 use std::sync::{Arc, Mutex};
 use std::thread;
 
